b517efa2ec7761734b2d0ab87c5549eed63f20e6
Ugan Sivagnanenthirarajah <usivagna@uwaterloo.ca>
2016-03-20 02:39:26 -0400

    Finalized Request script to Messaging Server
    Sends connected MAC, IP, SIGNAL

--- hardware/wifi.sh
+++ hardware/wifi.sh
@@ -1,13 +1,16 @@
 # Local reproduction of WiFi access script
-configure_edison --wifi
+
 
 curl -H "Content-Type: application/json" -X POST -d '{"wifiAccessPoints": [{"macAddress": "fc:c2:de:32:a4:bf"}]}' -o location.json  https://www.googleapis.com/geolocation/v1/geolocate\?key\=AIzaSyBIJxVeb8GOebSNEEC_pjOUKEKaYhPVvus
-jsonData="number=+14167990397"
-sendData="$jsonData, $(sed '1d;$d' location.json)"
-sendData=${sendData//$'\n'/}
-echo $sendData
+number="4167990397"
 
-curl -H "Content-Type: application/json" -X POST -d '$sendData'  http://22ab2fae.ngrok.io/alert_helper
+IP=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+MAC=$(ip link show wlan0 | awk '/ether/ {print $2}')
+iwconfig wlan0 | grep -i signal > signal.stdout
+cat signal.stdout
+temp=$(awk '{gsub(/dBm/,"");print}' signal.stdout)
+echo "$temp" >>temp.out
+SIGNAL=$(echo $temp |awk '{gsub(/.*Signal/,"");print}' temp.out)
 
 
-
+curl -G http://22ab2fae.ngrok.io/alert_helper\?ip\=$IP\&mac\=$MAC\&number\=$number\&signal\=\$SIGNAL
