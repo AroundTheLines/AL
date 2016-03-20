1d464343bf5e314302453752a2eb603e0cb37a8a
Will Liu <williamliu52@gmail.com>
2016-03-20 00:22:46 -0400

    Created file that has open network names

--- hardware/scan.sh
+++ hardware/scan.sh
@@ -7,10 +7,21 @@ wpa_cli -i wlan0 scan
 # Create file to store scanned networks and display file path
 OUTPUT="$(wpa_cli -i wlan0 scan_results)"
 destdir="networks.txt"
-# Awk test
+echo "${OUTPUT}" > "$destdir"
+
+# Prints out list of ESS networks with all the info
 awk ' /WPA/ {next} /WEP/ {next} /WPS/ {next} {print}' networks.txt
+
+# Counts number of ESS networks 
 awk 'BEGIN{total=-1} /WPA/ {next} /WEP/ {next} /WPS/ {next} {total += 1} END{}' networks.txt
+
+# Output names of ESS networks
 awk '/ssid/ {next} /WPA/ {next} /WEP/ {next} /WPS/ {next} {gsub(/.*\[ESS\]/,"");print}' networks.txt
+
+# Create file of only network names
+OUTPUT="$(awk '/ssid/ {next} /WPA/ {next} /WEP/ {next} /WPS/ {next} {gsub(/.*\[ESS\]/,"");print}' networks.txt)"
+destdir="open_networks.txt"
+echo "${OUTPUT}" > "$destdir" 
 
 # Script to connect to each open network
 #for i in {0..$total}
@@ -21,3 +32,9 @@ awk '/ssid/ {next} /WPA/ {next} /WEP/ {next} /WPS/ {next} {gsub(/.*\[ESS\]/,"");
         #wpa_cli -iwlan0 set_network 0 key_mgmt NONE
         #wpa_cli -iwlan0 set_network 0 mode 0
         #wpa_cli -iwlan0 set_network 0 ssid '"Bandipur"'
+	#wpa_cli -iwlan0 select_network 0                                        
+        #wpa_cli -iwlan0 enable_network 0                                        
+        #wpa_cli -iwlan0 reassociate                                             
+#done                                                                            
+                                                                                
+#wpa_cli -iwlan0 status
