a7526577044b4494ba0bc415d9702930ae8b164d
Ugan Sivagnanenthirarajah <usivagna@uwaterloo.ca>
2016-03-19 20:00:59 -0400

    Update Wifi

--- hardware/wifi.sh
+++ hardware/wifi.sh
@@ -2,9 +2,10 @@
 configure_edison --wifi
 
 curl -H "Content-Type: application/json" -X POST -d '{"wifiAccessPoints": [{"macAddress": "fc:c2:de:32:a4:bf"}]}' -o location.json  https://www.googleapis.com/geolocation/v1/geolocate\?key\=AIzaSyBIJxVeb8GOebSNEEC_pjOUKEKaYhPVvus
-jsonData="{number='+14167990397'},"
-locationData=$(<location.json)
-sendData="{$jsonData,$locationData}"
+jsonData="number=+14167990397"
+sendData="$jsonData, $(sed '1d;$d' location.json)"
+sendData=${sendData//$'\n'/}
+echo $sendData
 
 curl -H "Content-Type: application/json" -X POST -d '$sendData'  http://22ab2fae.ngrok.io/alert_helper
 
