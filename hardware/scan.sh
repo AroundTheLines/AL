3694afa36ae65f8f3c5119cd913b6da396a845a9
Will Liu <williamliu52@gmail.com>
2016-03-19 23:48:27 -0400

    script to scan for wifi networks and parse networks

---
+++ hardware/scan.sh
@@ -0,0 +1,23 @@
+# Turns on wpa_supplicant to /Users/Will enable wpa_cli
+systemctl enable  wpa_supplicant
+
+# Use wpa_cli to scan for networks
+wpa_cli -i wlan0 scan
+
+# Create file to store scanned networks and display file path
+OUTPUT="$(wpa_cli -i wlan0 scan_results)"
+destdir="networks.txt"
+# Awk test
+awk ' /WPA/ {next} /WEP/ {next} /WPS/ {next} {print}' networks.txt
+awk 'BEGIN{total=-1} /WPA/ {next} /WEP/ {next} /WPS/ {next} {total += 1} END{}' networks.txt
+awk '/ssid/ {next} /WPA/ {next} /WEP/ {next} /WPS/ {next} {gsub(/.*\[ESS\]/,"");print}' networks.txt
+
+# Script to connect to each open network
+#for i in {0..$total}
+        #wpa_cli -iwlan0 disconnect
+        #for i in `wpa_cli -iwlan0 list_networks | grep ^[0-9] | cut -f1`; do wpa_cli -iwlan0 remove_network $i; done
+        #wpa_cli -iwlan0 add_network
+        #wpa_cli -iwlan0 set_network 0 auth_alg OPEN
+        #wpa_cli -iwlan0 set_network 0 key_mgmt NONE
+        #wpa_cli -iwlan0 set_network 0 mode 0
+        #wpa_cli -iwlan0 set_network 0 ssid '"Bandipur"'
